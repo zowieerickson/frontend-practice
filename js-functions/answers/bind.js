071199125e611555c3e2c4542ffd96ba2242f095
Zowie Erickson <zowie.erickson@gmail.com>
2025-11-30 15:44:23 -0800

    Add bind solution

---
+++ js-functions/answers/bind.js
@@ -0,0 +1,17 @@
+// Implement Function.prototype.bind()
+
+const foo = function() {
+    console.log(this.bar);
+}
+
+let baz = foo.bind({bar: 'hello'})
+
+baz(); // Hello
+
+Function.prototype.bind = function(context) {
+    const fn = this;
+
+    return function() {
+        fn.call(context)
+    }
+}
